340c2fa290862395a46ba2f599f0907c3a91bfa3
Amjad Alsharafi <amjadsharafi10@gmail.com>
2020-05-25 11:10:04 +0800

    Add: drawing a line and the angle of it

--- src/main/java/ui/DrawingCanvas.java
+++ src/main/java/ui/DrawingCanvas.java
@@ -1,10 +1,23 @@
 package ui;
 
+import utils.Vector;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 
-public class DrawingCanvas extends JPanel {
+public class DrawingCanvas extends JPanel implements MouseListener, MouseMotionListener {
     long fps = 0;
+    private Vector mouseStart = new Vector(0d, 0d);
+    private Vector mouseCurrent = new Vector(0d, 0d);
+    private boolean dragging = false;
+
+    public DrawingCanvas() {
+        addMouseListener(this);
+        addMouseMotionListener(this);
+    }
 
     public void render(double delta, long fps) {
         prepareRender(delta);
@@ -27,9 +40,50 @@ public class DrawingCanvas extends JPanel {
 
     private void innerRender(Graphics2D g) {
         drawFPS(g);
+        drawLineAndAngle(g);
     }
 
     private void drawFPS(Graphics2D g) {
         g.drawString(String.valueOf(fps), 0, 10);
+    }
+
+    private void drawLineAndAngle(Graphics2D g) {
+        if (!dragging)
+            return;
+        g.drawLine((int) mouseStart.x, (int) mouseStart.y, (int) mouseCurrent.x, (int) mouseCurrent.y);
+        g.drawString(String.valueOf(mouseCurrent.sub(mouseStart).flipY().angleDeg()), 0, 30);
+    }
+
+    @Override
+    public void mousePressed(MouseEvent mouseEvent) {
+        dragging = true;
+        mouseStart.setValue(mouseEvent.getPoint());
+        mouseCurrent.setValue(mouseEvent.getPoint());
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent mouseEvent) {
+        dragging = false;
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent mouseEvent) {
+        mouseCurrent.setValue(mouseEvent.getPoint());
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent mouseEvent) {
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent mouseEvent) {
+    }
+
+    @Override
+    public void mouseExited(MouseEvent mouseEvent) {
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent mouseEvent) {
     }
 }
---
+++ src/main/java/utils/Vector.java
@@ -0,0 +1,44 @@
+package utils;
+
+import java.awt.*;
+
+public class Vector {
+    public double x, y;
+
+    public Vector(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void setValue(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Vector add(Vector other) {
+        return new Vector(x + other.x, y + other.y);
+    }
+
+    public Vector sub(Vector other) {
+        return new Vector(x - other.x, y - other.y);
+    }
+
+    public double angleRad() {
+        return Math.atan(y / x) + (x < 0 ? Math.PI : 0);
+    }
+
+    public double angleDeg() {
+        return angleRad() / Math.PI * 180;
+    }
+
+    public Vector flipY() {
+        y = -y;
+        // used for chaining.
+        return this;
+    }
+
+    public void setValue(Point point) {
+        x = point.x;
+        y = point.y;
+    }
+}
